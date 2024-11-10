355982be02720dae08bcb74515b2305b962f1ecf
shukurullo <burxonovshulkurullo16@gmail.com>
2024-11-10 10:51:55 +0500

    user profile image togirlandi

--- resources/views/usersprofile.blade.php
+++ resources/views/usersprofile.blade.php
@@ -50,7 +50,7 @@
                 @foreach ($user->posts as $post)
 
                     <div class="bg-white p-6 rounded-lg shadow-md">
-                        <img src="{{asset('/storage' . '/' . $post->image_path)}}" alt="Post Image"
+                        <img src="{{asset('/storage' . '/' . $post->image->image_path)}}" alt="Post Image"
                         class="w-full h-48 object-cover rounded-lg mb-4">
                         <h3 class="text-xl font-bold mb-2">{{$post->title}}</h3>
                         <p class="text-gray-700 mb-4">{{$post->description}}</p>
